be6bb1d96de533e7add4481e4c21623ba70f2740
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2025-03-02 08:28:58 +0530

    bug fixed

--- lockncode_abhishek/src/components/ui/progress.jsx
+++ lockncode_abhishek/src/components/ui/progress.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 import { cn } from "@/lib/utils";
 
-const Progress = ({ className, value, ...props }) => (
+const Progress = ({ className, value, indicatorClassName, ...props }) => (
   <ProgressPrimitive.Root
     data-slot="progress"
     className={cn(
@@ -15,7 +15,10 @@ const Progress = ({ className, value, ...props }) => (
   >
     <ProgressPrimitive.Indicator
       data-slot="progress-indicator"
-      className="bg-primary h-full w-full flex-1 transition-all"
+      className={cn(
+        "bg-primary h-full w-full flex-1 transition-all",
+        indicatorClassName
+      )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
